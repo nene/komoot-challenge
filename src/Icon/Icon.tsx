0ac8782a75b6aa4f0888f596309aaa22b7a38f0a
Rene Saarsoo <nene@triin.net>
2020-10-19 08:50:18 +0300

    Extract DeleteButton component

--- src/Icon/Icon.tsx
+++ src/Icon/Icon.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Icon.css";
 
-export const Icon: React.FC<{ name: "draggable" | "delete" }> = ({ name }) => {
-  return <span className={"icon-" + name} />;
+export const Icon: React.FC<{ name: "draggable" | "delete"; className?: string }> = ({ name, className = "" }) => {
+  return <span className={"icon-" + name + " " + className} />;
 };
--- src/List.tsx
+++ src/List.tsx
@@ -2,15 +2,21 @@ import React from "react";
 import { Icon } from "./Icon/Icon";
 import "./List.css";
 
+const DeleteButton: React.FC<{}> = () => (
+  <button className="DeleteButton">
+    <Icon name="delete" />
+  </button>
+);
+
 export const List: React.FC<{}> = () => {
   const waypoints = [1, 2, 3, 4];
   return (
     <ul className="List">
       {waypoints.map((wp) => (
         <li className="List_item" key={wp}>
-          <Icon name="draggable" />
+          <Icon name="draggable" className="List_item_grip" />
           <span>Waypoint {wp}</span>
-          <Icon name="delete" />
+          <DeleteButton />
         </li>
       ))}
     </ul>
