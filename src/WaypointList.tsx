6597ebcff825063f86e204e4cd9b4a61cee0fd5c
Rene Saarsoo <nene@triin.net>
2020-10-19 17:47:30 +0300

    Initial drag-drop implementation

--- src/WaypointList.tsx
+++ src/WaypointList.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback } from "react";
 import * as Leaflet from "leaflet";
 import { Icon } from "./Icon/Icon";
-import { deleteAt } from "./immutable-utils";
+import { deleteAt, moveIndex } from "./immutable-utils";
 import "./WaypointList.css";
 
 const DeleteButton: React.FC<{ onClick: () => void }> = ({ onClick }) => (
@@ -10,11 +10,50 @@ const DeleteButton: React.FC<{ onClick: () => void }> = ({ onClick }) => (
   </button>
 );
 
-const WaypointListItem: React.FC<{ index: number; onDelete: (index: number) => void }> = ({ index, onDelete }) => {
+export interface WaypointListItemProps {
+  index: number;
+  onDelete: (index: number) => void;
+  onMove: (oldIndex: number, newIndex: number) => void;
+}
+
+const WaypointListItem: React.FC<WaypointListItemProps> = ({ index, onDelete, onMove }) => {
   const handleDelete = useCallback(() => onDelete(index), [index, onDelete]);
+  const handleDragStart = useCallback(
+    (event: React.DragEvent<HTMLLIElement>) => {
+      event.dataTransfer.dropEffect = "move";
+      event.dataTransfer.setData("text/plain", String(index));
+    },
+    [index],
+  );
+  const handleDragEnter = useCallback((event: React.DragEvent<HTMLLIElement>) => {
+    event.currentTarget.classList.add("WaypointListItem-drop-target");
+  }, []);
+  const handleDragExit = useCallback((event: React.DragEvent<HTMLLIElement>) => {
+    event.currentTarget.classList.remove("WaypointListItem-drop-target");
+  }, []);
+  const handleDragOver = useCallback((event: React.DragEvent<HTMLLIElement>) => {
+    event.preventDefault();
+  }, []);
+  const handleDrop = useCallback(
+    (event: React.DragEvent<HTMLLIElement>) => {
+      event.preventDefault();
+      event.currentTarget.classList.remove("WaypointListItem-drop-target");
+      const newIndex = Number(event.dataTransfer.getData("text/plain"));
+      onMove(index, newIndex);
+    },
+    [index, onMove],
+  );
 
   return (
-    <li className="WaypointListItem">
+    <li
+      className="WaypointListItem"
+      draggable
+      onDragStart={handleDragStart}
+      onDragEnter={handleDragEnter}
+      onDragExit={handleDragExit}
+      onDragOver={handleDragOver}
+      onDrop={handleDrop}
+    >
       <Icon name="draggable" className="WaypointListItem_grip" />
       <span>Waypoint {index + 1}</span>
       <DeleteButton onClick={handleDelete} />
@@ -35,10 +74,17 @@ export const WaypointList: React.FC<WaypointListProps> = ({ waypoints, onChange 
     [waypoints, onChange],
   );
 
+  const onMove = useCallback(
+    (oldIndex: number, newIndex: number) => {
+      onChange(moveIndex(oldIndex, newIndex, waypoints));
+    },
+    [waypoints, onChange],
+  );
+
   return (
     <ul className="WaypointList">
       {waypoints.map((wp, i) => (
-        <WaypointListItem key={i} index={i} onDelete={onDelete} />
+        <WaypointListItem key={i} index={i} onDelete={onDelete} onMove={onMove} />
       ))}
     </ul>
   );
--- src/immutable-utils.ts
+++ src/immutable-utils.ts
@@ -9,3 +9,24 @@ export const updateAt = <T>(index: number, item: T, array: T[]): T[] => [
   item,
   ...array.slice(index + 1),
 ];
+
+export const insertAt = <T>(index: number, item: T, array: T[]): T[] => [
+  ...array.slice(0, index),
+  item,
+  ...array.slice(index),
+];
+
+export const moveIndex = <T>(fromIndex: number, toIndex: number, array: T[]): T[] => {
+  const item = array[fromIndex];
+
+  if (fromIndex < toIndex) {
+    // moving forwards: first add new, then remove old
+    return deleteAt(fromIndex, insertAt(toIndex, item, array));
+  } else if (fromIndex > toIndex) {
+    // moving backwards: first remove old, then insert new
+    return insertAt(toIndex, item, deleteAt(fromIndex, array));
+  } else {
+    // no change
+    return array;
+  }
+};
