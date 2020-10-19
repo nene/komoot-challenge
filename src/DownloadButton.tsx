c1148b01120499edb89000a45adc789139a569f7
Rene Saarsoo <nene@triin.net>
2020-10-19 21:40:49 +0300

    Use data-url to download GPX file

--- src/DownloadButton.tsx
+++ src/DownloadButton.tsx
@@ -1,16 +1,15 @@
-import React, { useCallback } from "react";
+import React from "react";
 import * as Leaflet from "leaflet";
 import "./DownloadButton.css";
 import { createGpx } from "./gpx";
 
 export const DownloadButton: React.FC<{ waypoints: Leaflet.LatLng[] }> = ({ waypoints }) => {
-  const handleClick = useCallback(() => {
-    console.log(createGpx(waypoints));
-  }, [waypoints]);
-
   return (
-    <button className="DownloadButton" onClick={handleClick}>
+    // eslint-disable-next-line react/jsx-no-target-blank
+    <a href={createXmlDataUrl(createGpx(waypoints))} className="DownloadButton" target="_blank" download="route.gpx">
       Download your Route
-    </button>
+    </a>
   );
 };
+
+const createXmlDataUrl = (data: string): string => URL.createObjectURL(new Blob([data], { type: "application/xml" }));
