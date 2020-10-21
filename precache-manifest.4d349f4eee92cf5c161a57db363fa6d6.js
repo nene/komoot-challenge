ce3ed9ce5d4213c929bb4e0cd3bc2d49d4cb073a
Rene Saarsoo <nene@triin.net>
2020-10-21 10:57:21 +0300

    Updates

---
+++ precache-manifest.4d349f4eee92cf5c161a57db363fa6d6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfe2fd6c4c89dfa60d5925d9e765a3b8",
+    "url": "/komoot-challenge/index.html"
+  },
+  {
+    "revision": "d5ef4dec4a182ca20eee",
+    "url": "/komoot-challenge/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "a3777d9cae3625525952",
+    "url": "/komoot-challenge/static/css/main.e15b6533.chunk.css"
+  },
+  {
+    "revision": "d5ef4dec4a182ca20eee",
+    "url": "/komoot-challenge/static/js/2.ceec4f98.chunk.js"
+  },
+  {
+    "revision": "5c6a78c1e91cfdbd9bac885aa4d8ecaf",
+    "url": "/komoot-challenge/static/js/2.ceec4f98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3777d9cae3625525952",
+    "url": "/komoot-challenge/static/js/main.53be0ad4.chunk.js"
+  },
+  {
+    "revision": "58dfc0167ba7beab5043",
+    "url": "/komoot-challenge/static/js/runtime-main.ed8186ad.js"
+  },
+  {
+    "revision": "58c82e3b685510cc0ca08d92e5c5edad",
+    "url": "/komoot-challenge/static/media/icofont.58c82e3b.woff"
+  },
+  {
+    "revision": "bf8ef9215e9c49234d8beee82c4a3896",
+    "url": "/komoot-challenge/static/media/icofont.bf8ef921.woff2"
+  }
+]);
